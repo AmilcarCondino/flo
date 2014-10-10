720298df47c47962a60e69cb207608081af57bd5
Amilcar Condino <amilcar.condino@gmail.com>
2014-10-10 16:23:03 -0300

    Css

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -13,40 +13,49 @@
 
     </head>
     <body>
+
+        <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/">C.R.U.D</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li>{{ HTML::link('/', 'Home') }}</li>
+                        <li>{{ HTML::link('posts', 'Posts') }}</li>
+                        <li>{{ HTML::link('photos', 'Fotos') }}</li>
+                        <li>{{ HTML::link('login', 'Log in') }}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+
         <div class="container">
-            <div class="navbar navbar-default" role="navigation">
-                <ul class="nav nav-pills">
-                    <li>{{ HTML::link('/', 'Home') }}</li>
-                    <li>{{ HTML::link('posts', 'Posts') }}</li>
-                    <li>{{ HTML::link('photos', 'Fotos') }}</li>
-                    <li>{{ HTML::link('login', 'Log in') }}</li>
-                </ul>
+            @if (Session::has('flash_message'))
+            <div class="alert {{ Session::get('flash_type') }} " role="alert">
+                {{ Session::get('flash_message') }}
+            </div>
+            @endif
+
+            @yield('content')
+        </div>
+        <div class="footer">
+            <div class="container">
+                <p class="text-muted">Just a c.r.u.d, by KeleK.</p>
             </div>
         </div>
-    <div class="container">
-        @if (Session::has('flash_message'))
-        <div class="alert {{ Session::get('flash_type') }} " role="alert">
-            {{ Session::get('flash_message') }}
-        </div>
-        @endif
 
-        @yield('content')
-    </div>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12 text-center">
-                <footer>
-                    Just a c.r.u.d, by KeleK.
-                </footer>
-            </div>
-        </div>
-    </div>
+            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+            <!-- Include all compiled plugins (below), or include individual files as needed -->
+            <script src="/js/bootstrap.min.js"></script>
     </body>
-
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="/js/bootstrap.min.js"></script>
-
 </html>
