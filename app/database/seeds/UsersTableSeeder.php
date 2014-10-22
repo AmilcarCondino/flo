172d9c25f3fa4e93a42a495caf9e40095d9594df
Amilcar Condino <amilcar.condino@gmail.com>
2014-10-22 18:01:35 -0300

    Seed admin table

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -1,29 +1,26 @@
 <?php
 
-// Composer: "fzaninotto/faker": "v1.3.0"
-use Faker\Factory as Faker;
 
 class UsersTableSeeder extends Seeder {
 
 	public function run()
 	{
-		$faker = Faker::create();
 
-		foreach(range(1, 10) as $index)
-		{
-			User::create([
+        Eloquent::unguard();
 
-                'username' => 'kelek',
-                'password' => Hash::make('456'),
-                'email' => 'feakelek@gmail.com',
-                'rol' => 'admin',
-                'first_name' => 'Amilcar',
-                'country' => 'Argentina',
-                'city' => 'CABA',
-                'gender' => 'Masculino'
+        User::create([
 
-			]);
-		}
-	}
+            'username' => 'kelek',
+            'password' => Hash::make('456'),
+            'email' => 'feakelek@gmail.com',
+            'rol' => 'admin',
+            'first_name' => 'Amilcar',
+            'country' => 'Argentina',
+            'city' => 'CABA',
+            'gender' => 'Masculino',
+
+        ]);
+    }
+
 
 }
