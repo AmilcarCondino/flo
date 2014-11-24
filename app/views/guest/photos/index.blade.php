b228441741a49be36df6c90b1234b915ab5bfc04
Amilcar Condino <amilcar.condino@gmail.com>
2014-11-24 18:51:16 -0300

    bug fix

--- app/views/guest/photos/index.blade.php
+++ app/views/guest/photos/index.blade.php
@@ -1,17 +1,17 @@
 <?php $pattern = [null, 'w2 h2', 'h3', 'h2', 'w3', null, null, 'h2', 'w2 h3', null, 'h2', null, 'w2 h2', 'w2', null, 'h2', null, null, 'h3', 'h2', null, null, 'h2']; ?>
 
 @section('content')
+
 <div id="masonry-container">
 
-    <?php $i = 'a'; ?>
     @for($i = 0; $i < count($photos); $i++)
-        @if($photos[$i]->show === 1)
-            <div class="item {{ $pattern[ ($i % count($pattern) ) ] }}">
-<!--                <a href="/photos/{{ $photos[$i]->id }}">-->
-                    <img src="/uploads/images/{{$photos[$i]->img_name}}" alt="alt">
-<!--                </a>-->
-            </div>
-        @endif
+    @if($photos[$i]->show === 1)
+    <div class="item {{ $pattern[ ($i % count($pattern) ) ] }}">
+        <!--                <a href="/photos/{{ $photos[$i]->id }}">-->
+        <img src="/uploads/images/{{$photos[$i]->img_name}}" alt="alt">
+        <!--                </a>-->
+    </div>
+    @endif
     @endfor
 </div>
 
@@ -19,9 +19,9 @@
     $( window ).load(function(){
         var container = document.querySelector('#masonry-container');
         var msnry = new Masonry( container, {
-          // options
-          columnWidth: 190,
-          itemSelector: '.item'
+            // options
+            columnWidth: 190,
+            itemSelector: '.item'
         });
 
         $('.item').each(function(){
