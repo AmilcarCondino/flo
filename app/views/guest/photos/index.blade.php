bbb01c1c1b4f70d372d69ef3f7906e1518989a91
Amilcar Condino <amilcar.condino@gmail.com>
2014-12-11 13:10:12 -0300

    Image Gallery patern

--- app/views/guest/photos/index.blade.php
+++ app/views/guest/photos/index.blade.php
@@ -1,4 +1,4 @@
-<?php $pattern = [null, 'w2 h2', 'h3', 'h2', 'w3', null, null, 'h2', 'w2 h3', null, 'h2', null, 'w2 h2', 'w2', null, 'h2', null, null, 'h3', 'h2', null, null, 'h2']; ?>
+<?php $pattern = ['w1', 'w2', 'w3', 'w4','w5', 'w6', 'w7', 'w8', 'w9', 'w10', 'w11']; ?>
 
 @section('content')
 
@@ -7,9 +7,9 @@
     @for($i = 0; $i < count($photos); $i++)
     @if($photos[$i]->show === 1)
     <div class="picture {{ $pattern[ ($i % count($pattern) ) ] }}">
-        <!--                <a href="/photos/{{ $photos[$i]->id }}">-->
-        <img src="/uploads/images/{{$photos[$i]->img_name}}" alt="alt">
-        <!--                </a>-->
+        <a href="/photos/{{ $photos[$i]->id }}">
+            <img src="/uploads/images/{{$photos[$i]->img_name}}" alt="alt">
+        </a>
     </div>
     @endif
     @endfor
@@ -46,23 +46,23 @@
             }
         });
 
-        $('.picture').click(function(){
-            $(this).toggleClass('gigante');
-            var img = $(this).find('img');
-            img.removeAttr('style');
-            var tall_img = (img.height() > img.width());
-
-            if(tall_img){
-                img.css({'max-width': $(this).width()});
-                var margin = ( img.height()-$(this).height() )/2;
-                img.css({'margin-top': -margin});
-            }else{
-                img.css({'max-height': $(this).height()});
-                var margin = ( img.width()-$(this).width() )/2;
-                img.css({'margin-left': -margin});
-            }
-            msnry.layout();
-        });
+//        $('.picture').click(function(){
+//            $(this).toggleClass('gigante');
+//            var img = $(this).find('img');
+//            img.removeAttr('style');
+//            var tall_img = (img.height() > img.width());
+//
+//            if(tall_img){
+//                img.css({'max-width': $(this).width()});
+//                var margin = ( img.height()-$(this).height() )/2;
+//                img.css({'margin-top': -margin});
+//            }else{
+//                img.css({'max-height': $(this).height()});
+//                var margin = ( img.width()-$(this).width() )/2;
+//                img.css({'margin-left': -margin});
+//            }
+//            msnry.layout();
+//        });
 
     });
 
