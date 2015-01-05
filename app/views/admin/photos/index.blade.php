6a8b64114d897d4a7e20eb280daca39fe9b73de0
Amilcar Condino <amilcar.condino@gmail.com>
2015-01-05 09:53:19 -0300

    Styling foto.admin

--- app/views/admin/photos/index.blade.php
+++ app/views/admin/photos/index.blade.php
@@ -20,59 +20,61 @@
                         <th>Categoria</th>
                         <th>Etiqueta</th>
                         <th>Creacion</th>
-                        <th>Editar</th>
-                        <th>Eliminar</th>
                     </tr>
                 </thead>
                     <tbody>
                         @foreach($photos as $photo)
                             <tr>
-                                <th>
+                                <td>
                                     <a href="/admin/photos/{{ $photo->id }}">
-                                        <img src="/uploads/images/{{$photo->img_name}}"class="img-responsive img-rounded" alt="Responsive image" style=" width: 250px">
+                                        <img src="/uploads/images/{{$photo->img_name}}"class="img-responsive img-rounded" alt="Responsive image" style=" height: 50px">
                                     </a>
-                                </th>
-                                <th>
+                                </td>
+                                <td>
                                     {{$photo->title}}
-                                </th>
-                                <th>
+                                </td>
+                                <td>
                                     {{$photo->description}}
-                                </th>
-                                <th>
+                                </td>
+                                <td>
                                     @if($photo->show === 1)
                                         Si
                                     @else
                                         No
                                     @endif
-                                </th>
-                                <th>
+                                </td>
+                                <td>
                                     @foreach($photo->collections as $collection)
                                         {{ $collection->title }}
                                     @endforeach
-                                </th>
-                                <th>
+                                </td>
+                                <td>
                                     @foreach($photo->categories as $category)
                                         {{ $category->title }}
                                     @endforeach
-                                </th>
-                                <th>
+                                </td>
+                                <td>
                                     @foreach($photo->tags as $tag)
                                         {{ $tag->title }}
                                     @endforeach
-                                </th>
-                                <th>
+                                </td>
+                                <td>
                                     {{$photo->created_at}}
-                                </th>
-                                <th>
-                                    <a class="btn btn-xs btn-success" href="{{ route('admin.photos.edit', $photo->id) }}" role="button">Editar</a>
-                                </th>
-                                <th>
+                                </td>
+                                <td>
+                                    {{ Form::open(['method' => 'get', 'route' => ['admin.photos.edit', $photo->id]]) }}
+
+                                        {{ Form::submit('Editar', array('class'=>'btn btn-sm btn-primary')) }}
+
+                                    {{ Form::close() }}
+                                </td>
+                                <td>
                                     {{ Form::open(['method' => 'delete', 'route' => ['admin.photos.destroy', $photo->id]]) }}
 
                                         {{ Form::submit('Eliminar', array('class'=>'btn btn-xs btn-danger')) }}
 
                                     {{ Form::close() }}
-                                </th>
+                                </td>
 
                             </tr>
                         @endforeach
