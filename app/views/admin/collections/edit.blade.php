3ea3b0a315b85ca0f3cbf35b697aabe8b547b789
Amilcar Condino <amilcar.condino@gmail.com>
2015-01-05 08:39:14 -0300

    Indention correcion

--- app/views/admin/collections/edit.blade.php
+++ app/views/admin/collections/edit.blade.php
@@ -2,11 +2,11 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-4">
-            <h1>Editar registro</h1>
+            <h1>Editar Colecion</h1>
         </div>
     </div>
     <div class="row">
-        <div class="col-sm-4">
+        <div class="col-sm-6">
             {{  Form::model($collection, ['method' => 'PATCH', 'route' => ['admin.collections.update', $collection->id]]) }}
 
                 {{ Form::openGroup('title', 'Titulo: ') }}<br />
@@ -15,10 +15,10 @@
 
                 <div class="row">
                     <div class="col-sm-6">
-                    {{ Form::submit('Editar Coleccion', array('class'=>'btn btn-sm btn-success')) }}
+                        {{ Form::submit('Editar', array('class'=>'btn btn-sm btn-success')) }}
                     </div>
                     <div class="col-sm-6">
-                    <a class="btn btn-block btn-danger" href="{{ url('admin') }}" role="button">Cancelar</a>
+                        <a class="btn btn-block btn-danger" href="{{ url('admin') }}" role="button">Cancelar</a>
                     </div>
                 </div>
 
--- app/views/admin/tags/edit.blade.php
+++ app/views/admin/tags/edit.blade.php
@@ -2,11 +2,11 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-4">
-            <h1>Editar registro</h1>
+            <h1>Editar Etiqueta</h1>
         </div>
     </div>
     <div class="row">
-        <div class="col-sm-4">
+        <div class="col-sm-6">
             {{  Form::model($tag, ['method' => 'PATCH', 'route' => ['admin.tags.update', $tag->id]]) }}
 
                 {{ Form::openGroup('title', 'Titulo: ') }}<br />
@@ -15,10 +15,10 @@
 
                 <div class="row">
                     <div class="col-sm-6">
-                    {{ Form::submit('Editar etiqueta', array('class'=>'btn btn-sm btn-success')) }}
+                        {{ Form::submit('Editar', array('class'=>'btn btn-sm btn-success')) }}
                     </div>
                     <div class="col-sm-6">
-                    <a class="btn btn-block btn-danger" href="{{ url('admin') }}" role="button">Cancelar</a>
+                        <a class="btn btn-block btn-danger" href="{{ url('admin') }}" role="button">Cancelar</a>
                     </div>
                 </div>
 
