fa480dfe520efcf50b241b95c1372840d1259efb
Amilcar Condino <amilcar.condino@gmail.com>
2015-01-24 20:13:52 -0300

    Filthers with navbar

--- app/views/guest/photos/index.blade.php
+++ app/views/guest/photos/index.blade.php
@@ -2,20 +2,18 @@
 
 @section('content')
 <div class="navbar navbar-inverse" >
-<div class="navbar-form navbar-right">
-    {{ Form::open(array('url' => 'photos', 'method' => 'get', 'id' => 'filter', 'class' => 'form-group')) }}
+    <div class="col-sm-3 navbar-form navbar-right">
+        {{ Form::open(array('url' => 'photos', 'method' => 'get', 'id' => 'filter', 'class' => 'form-group')) }}
 
-        {{ Form::openGroup('filter') }}
-            {{ Form::select('categories', array('' => 'Categorias', 'Selecione para filtrar' => $categories), array('id' => 'tags', 'class' => 'selectpicker')) }}
-        {{ Form::closeGroup() }}
+            {{ Form::openGroup('filter') }}
+                {{ Form::select('categories', array('' => 'Categorias', 'Selecione para filtrar' => $categories), array('id' => 'tags', 'class' => 'selectpicker')) }}
+            {{ Form::closeGroup() }}
 
-        {{ Form::openGroup('filter') }}
-            {{ Form::select('collections', array('' => 'Colecciones', 'Selecione para filtrar' => $collections), array('id' => 'tags', 'class' => 'selectpicker')) }}
-        {{ Form::closeGroup() }}
-
-
-    {{ Form::close() }}
-</div>
+            {{ Form::openGroup('filter') }}
+                {{ Form::select('collections', array('' => 'Colecciones', 'Selecione para filtrar' => $collections), array('id' => 'tags', 'class' => 'selectpicker')) }}
+            {{ Form::closeGroup() }}
+        {{ Form::close() }}
+    </div>
 </div>
 <div id="masonry-container" class="popup-gallery">
 <!--Pseudo randomizer of image size-->
