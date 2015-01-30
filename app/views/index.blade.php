d2c8c8611a29fe723f1880d3f89501f47afdd470
Amilcar Condino <amilcar.condino@gmail.com>
2015-01-30 16:19:46 -0300

    Just for marto

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -21,7 +21,7 @@
                     <li>
                         <div class="slide-body" data-group="slide">
                             <img src="/uploads/slider/{{ $slider->img_name }}">
-                            <div class="caption header" data-animate="slideAppearLeftToRight" data-delay="500" data-length="1500" style="top: 5%; left: 90%; margin-left: -100; margin-right: 100;">
+                            <div class="caption header" data-animate="slideAppearLeftToRight" data-delay="500" data-length="300" style="top: 5%; left: 90%; margin-left: -100; margin-right: 100;">
                                 <h3>{{ $slider->headline }}</h3>
                                 <div class="caption sub" data-animate="slideAppearUpToDown" data-delay="800" data-length="300">{{ $slider->paragraph }}</div>
                             </div>
